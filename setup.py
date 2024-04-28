4ede49c21bd54e15a3625e43f237ae0f40fcf724
Gadgetmies <71213783+gadgetmies@users.noreply.github.com>
2024-04-28 19:29:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name=CMD_NAME,
+    name='cables',
     version=__version__,
     author='Gadgetmies',
     #author_email='',
